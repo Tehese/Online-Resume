35bcf8bf39ec25854e27f67c3d71c23d607428ac
Tehese <Scanfiel@uccs.edu>
2023-06-23 11:47:28 -0600

    Initial Commit

---
+++ src/main/java/com/example/online_resume/OnlineResumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineResumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineResumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/online_resume/OnlineResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineResumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
