629b21028cfb9b20a41fb6512ccf67db2ab17ada
Tehese <Scanfiel@uccs.edu>
2023-07-03 13:43:33 -0600

    Push first test case

--- src/test/java/com/example/online_resume/OnlineResumeApplicationTests.java
+++ src/test/java/com/example/online_resume/OnlineResumeApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.online_resume;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+
 @SpringBootTest
 class OnlineResumeApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue("pass",true);
     }
 
 }
